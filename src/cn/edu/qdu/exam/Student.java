d080ebd516a339d3bff15c420739272c1afea1d2
NIIT <NIIT@406-0166>
2016-05-16 18:38:12 +0800

    Exam1.5

--- src/cn/edu/qdu/exam/Student.java
+++ src/cn/edu/qdu/exam/Student.java
@@ -8,33 +8,51 @@ import java.util.Iterator;
 import java.util.List;
 
 public class Student {
-    List<Integer> choice = new ArrayList<Integer>();
-    String name;
-    Integer score=0;
-    BufferedReader in= null;
-    public void readAnswer(BufferedReader in,String name) throws IOException{
-    	 this.name = name.substring(0,name.indexOf(".") );
-    	 int temp;
-    	 while ((temp=in.read())!=-1) {
+	List<Integer> choice = new ArrayList<Integer>();
+	private String name;
+	private Integer score = 0;
+	BufferedReader in = null;
+
+	public void readAnswer(BufferedReader in) throws IOException {
+		int temp;
+		while ((temp = in.read()) != -1) {
 			choice.add(temp);
 		}
-    }
-    public  void compare(List rightAnswer){
-    	Iterator<Integer> stu =  this.choice.iterator(); 
-    	Iterator<Integer> ans = rightAnswer.iterator();
-    	while (stu.hasNext()&&ans.hasNext()) {
-    		int a=stu.next();
-    		int b=ans.next();
-			if (a!=10&&a!=13&&b!=10&&b!=13&&a==b||a==b+32) {
-				score+=4;
+	}
+
+	public void compare(List rightAnswer) {
+		Iterator<Integer> stu = this.choice.iterator();
+		Iterator<Integer> ans = rightAnswer.iterator();
+		while (stu.hasNext() && ans.hasNext()) {
+			int a = stu.next();
+			int b = ans.next();
+			if (a != 10 && a != 13 && b != 10 && b != 13 && a == b || a == b + 32) {
+				score += 4;
 			}
 		}
-    	
-    }
-    public void recordResult(BufferedWriter out) throws IOException{
-    	out.write(name+":");
-    	out.write(score.toString()+"\r\n");
-    	System.out.println(name+" : "+score);
-    }
-  
+
+	}
+
+	public void recordResult(BufferedWriter out) throws IOException {
+		out.write(name + ":");
+		out.write(score.toString() + "\r\n");
+		System.out.println(name + " : " + score);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getScore() {
+		return score;
+	}
+
+	public void setScore(Integer score) {
+		this.score = score;
+	}
+
 }
--- src/cn/edu/qdu/exam/Teacher.java
+++ src/cn/edu/qdu/exam/Teacher.java
@@ -9,42 +9,43 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Teacher {
-public static void main(String[] args) throws IOException {
-	File students =new File("C:\\Users\\wang\\students");
-	File answer =new File("C:\\Users\\wang\\answer.txt");
-	File result = new File("C:\\Users\\wang\\result.txt");
-	if(!result.exists()){
-		result.createNewFile();
-	}
-	File[] menu = students.listFiles();
-	Student[] student = new Student[menu.length];
-	BufferedReader in = null;
-	BufferedWriter out = null ;
-	try{
-		Answer answer1=new Answer();
-		in = new BufferedReader(new FileReader(answer));
-		answer1.readAnswer(in);
-	for (int i = 0; i < menu.length; i++) {
-		student[i]=new Student();
-		in = new BufferedReader(new FileReader(menu[i]));
-		student[i].readAnswer(in, menu[i].getName());
-		student[i].compare(answer1.rightAnswer);
-		out = new BufferedWriter(new FileWriter(result,true));
-		student[i].recordResult(out);
-		in.close();
-		out.close();
-	}
-	}catch(Exception e){
-		e.printStackTrace();
-	}finally{
-		if(in!=null){
-			in.close();
+	public static void main(String[] args) throws IOException {
+		File students = new File("C:\\Users\\NIIT\\students");
+		File answer = new File("C:\\Users\\NIIT\\answer.txt");
+		File result = new File("C:\\Users\\NIIT\\result.txt");
+		if (!result.exists()) {
+			result.createNewFile();
 		}
-		if(out!=null){
-			out.close();
+		File[] menu = students.listFiles();
+		Student[] student = new Student[menu.length];
+		BufferedReader in = null;
+		BufferedWriter out = null;
+		try {
+			Answer answer1 = new Answer();
+			in = new BufferedReader(new FileReader(answer));
+			answer1.readAnswer(in);
+			for (int i = 0; i < menu.length; i++) {
+				student[i] = new Student();
+				in = new BufferedReader(new FileReader(menu[i]));
+				student[i].setName(menu[i].getName().substring(0, menu[i].getName().indexOf(".")));
+				student[i].readAnswer(in);
+				student[i].compare(answer1.rightAnswer);
+				out = new BufferedWriter(new FileWriter(result, true));
+				student[i].recordResult(out);
+				in.close();
+				out.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+			if (out != null) {
+				out.close();
+			}
+
 		}
-		
 	}
-}
 
 }
